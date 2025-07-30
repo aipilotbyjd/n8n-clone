8db0424446019585f9a31c134222051ffbbdc9e9
Jaydeep Dhrangiya <recentpagetoday@gmail.com>
2025-07-30 23:54:11 +0530

    sds

---
+++ apps/credentials-vault/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'credentials-vault',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/credentials-vault',
+};
